8a29763a704cea0f6df902b3341524fb83a10358
mohammad hindawash <djslakivich@mohammads-Air.home>
2023-06-27 16:07:30 -0400

    Process is added due to a terminal error I was receiving for one of the maps not containing a key for each part and I updated that. Updated the reviews to have a background, include a star rating system and adjusted the positioning of the form on PC and mobile.

--- components/Reviews.tsx
+++ components/Reviews.tsx
@@ -1,69 +1,84 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import ReactStars from "react-rating-stars-component";
 
 type Review = {
-  name: string;
-  content: string;
-  rating: number;
+    id: string;
+    name: string;
+    content: string;
+    rating: number;
 }
 
 const Reviews: React.FC = () => {
+  const [review, setReview] = useState<string>("");
   const [name, setName] = useState<string>("");
-  const [content, setContent] = useState<string>("");
   const [rating, setRating] = useState<number>(0);
   const [reviews, setReviews] = useState<Review[]>([]);
 
   useEffect(() => {
     const savedReviews = JSON.parse(localStorage.getItem('reviews') || '[]');
-    if (savedReviews.length > 0) {
+    if (savedReviews) {
       setReviews(savedReviews);
     }
   }, []);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    const newReviews = [{name, content, rating}, ...reviews];
-    setReviews(newReviews);
-    localStorage.setItem('reviews', JSON.stringify(newReviews));
-    setName('');
-    setContent('');
-    setRating(0);
+    if (name && review) {
+      const newReview = { id: `${Date.now()}-${reviews.length}`, name, content: review, rating };
+      const newReviews = [newReview, ...reviews];
+      setReviews(newReviews);
+      localStorage.setItem('reviews', JSON.stringify(newReviews));
+      setReview('');
+      setName('');
+      setRating(0);
+    } else {
+      alert("Please fill in all fields before submitting");
+    }
   }
 
   return (
-    <div className="flex flex-col lg:flex-row min-h-screen bg-gray-50 pt-20 lg:px-20 space-y-10 lg:space-y-0 lg:space-x-10">
-      <form onSubmit={handleSubmit} className="flex flex-col items-center space-y-5 bg-white p-6 rounded shadow-md w-full lg:w-1/2">
-        <h2 className="text-2xl mb-4">Leave us a review:</h2>
-        <input type="text" placeholder="Your Name" value={name} onChange={e => setName(e.currentTarget.value)} className="w-full p-2 border border-gray-300 rounded"/>
-        <textarea placeholder="Your Review" value={content} onChange={e => setContent(e.currentTarget.value)} className="w-full p-2 border border-gray-300 rounded h-20"/>
-        <ReactStars
-          count={5}
-          onChange={setRating}
-          size={24}
-          activeColor="#ffd700"
-        />
-        <input type="submit" value="Submit" className="px-4 py-2 bg-blue-500 text-white rounded"/>
-      </form>
-      <div className="bg-white p-6 rounded shadow-md w-full lg:w-1/2 overflow-auto">
-        <div>
-          <h2 className="text-2xl mb-4">Most Recent Review</h2>
-          {reviews[0] && 
-            <>
-              <h3 className="text-lg font-semibold">{reviews[0].name} - {Array(reviews[0].rating).fill('⭐').join('')}</h3>
-              <p>{reviews[0].content}</p>
-            </>
-          }
-        </div>
-        <div>
-          <h2 className="text-2xl mt-10 mb-4">All Reviews</h2>
-          {reviews.map((review, index) => 
-            index !== 0 && (
-              <div key={index} className="mt-4 border-t pt-4">
-                <h3 className="text-lg font-semibold">{review.name} - {Array(review.rating).fill('⭐').join('')}</h3>
-                <p>{review.content}</p>
+    <div className="flex flex-col lg:flex-row h-screen bg-transparent p-4 lg:p-10 overflow-hidden my-5">
+      <div className="lg:w-1/2 p-4 lg:pr-10 mt-10">
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label className="block">Name:</label>
+            <input type="text" value={name} onChange={e => setName(e.currentTarget.value)} className="w-full p-2 border rounded" />
+          </div>
+          <div>
+            <label className="block">Review:</label>
+            <textarea rows={5} value={review} onChange={e => setReview(e.currentTarget.value)} className="w-full p-2 border rounded" />
+          </div>
+          <div>
+            <label className="block">Rating:</label>
+            <ReactStars
+              count={5}
+              value={rating}
+              onChange={(newRating: number) => setRating(newRating)}
+              size={24}
+              activeColor="#ffd700"
+            />
+          </div>
+          <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded">Submit</button>
+        </form>
+      </div>
+      <div className="lg:w-1/2 p-4 lg:pl-10 flex flex-col">
+        <h2 className="text-2xl mb-4">Reviews</h2>
+        <div className="overflow-auto flex-grow space-y-4 h-64">
+          {reviews.map((review) => (
+            <div key={review.id} className="p-4 border rounded space-y-2 mb-4 bg-white">
+              <div>
+                <strong>{review.name}</strong>
+                <ReactStars
+                  count={5}
+                  value={review.rating}
+                  size={24}
+                  activeColor="#ffd700"
+                  edit={false}
+                />
               </div>
-            )
-          )}
+              <div>{review.content}</div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
