12eaeb4e70718c8933baaa976ce31898cbc43ba4
mohammad hindawash <djslakivich@mohammads-Air.home>
2023-06-30 16:26:12 -0400

    Rebased vote_for_us for main and fixed a few css styling issues that was caused by the rebase.

--- components/Process.tsx
+++ components/Process.tsx
@@ -13,7 +13,7 @@ function Process({}: Props) {
 			transition={{ duration: 1.5 }}
 			className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
 		>
-			<h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+			<h3 className="absolute top-24 uppercase tracking-[20px] text-black-500 text-2xl">
 				Our Process
 			</h3>
 
